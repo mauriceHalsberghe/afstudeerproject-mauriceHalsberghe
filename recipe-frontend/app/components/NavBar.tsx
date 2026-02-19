2f6257d4449e1e00e727c900a40b684e484d6ecb
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-02-19 16:36:22 +0100

    Frontend - added nav current page

--- recipe-frontend/app/components/NavBar.tsx
+++ recipe-frontend/app/components/NavBar.tsx
@@ -1,6 +1,8 @@
-import NavBarStyles from '@/app/styles/components/navbar.module.css';
+"use client";
 
+import NavBarStyles from '@/app/styles/components/navbar.module.css';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import HomeIcon from '@/public/home.svg'
 import AppleIcon from '@/public/apple.svg'
@@ -9,27 +11,46 @@ import CartIcon from '@/public/cart.svg'
 import ProfileIcon from '@/public/profile.svg'
 
 function NavBar() {
-  return (
-    <nav className={NavBarStyles.navbar}>
-        <ul className={NavBarStyles.navlist}>
-            <li className={NavBarStyles.navitem}>
-                <Link href={'./'}><HomeIcon />Home</Link>
-            </li>
-            <li className={NavBarStyles.navitem}>
-                <Link href={'./ingredients'}><AppleIcon />Ingedients</Link>
-            </li>
-            <li className={NavBarStyles.navitem}>
-                <Link href={'./liked-recipes'}><HeartIcon />Liked</Link>
-            </li>
-            <li className={NavBarStyles.navitem}>
-                <Link href={'./shopping-list'}><CartIcon />List</Link>
-            </li>
-            <li className={NavBarStyles.navitem}>
-                <Link href={'./profile'}><ProfileIcon />Profile</Link>
-            </li>
-        </ul>
-    </nav>
-  );
+    const pathname = usePathname();
+
+    const isActive = (path: string) => pathname === path;
+
+    return (
+        <nav className={NavBarStyles.navbar}>
+            <ul className={NavBarStyles.navlist}>
+                <li 
+                    className={`${NavBarStyles.navitem} ${
+                    isActive("/") ? NavBarStyles["navitem-current"] : ""}`}  
+                >
+                    <Link href={'./'}><HomeIcon />Home</Link>
+                </li>
+                <li 
+                    className={`${NavBarStyles.navitem} ${
+                    isActive("/ingredients") ? NavBarStyles["navitem-current"] : ""}`}  
+                >
+                    <Link href={'./ingredients'}><AppleIcon />Ingedients</Link>
+                </li>
+                <li 
+                    className={`${NavBarStyles.navitem} ${
+                    isActive("/liked-recipes") ? NavBarStyles["navitem-current"] : ""}`}  
+                >
+                    <Link href={'./liked-recipes'}><HeartIcon />Liked</Link>
+                </li>
+                <li 
+                    className={`${NavBarStyles.navitem} ${
+                    isActive("/shopping-list") ? NavBarStyles["navitem-current"] : ""}`}  
+                >
+                    <Link href={'./shopping-list'}><CartIcon />List</Link>
+                </li>
+                <li 
+                    className={`${NavBarStyles.navitem} ${
+                    isActive("/profile") ? NavBarStyles["navitem-current"] : ""}`}  
+                >
+                    <Link href={'./profile'}><ProfileIcon />Profile</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
