6b7e422a644d79198575acf4122d2fa5c7e60d0e
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-02-19 16:18:29 +0100

    Frontend - added navbar

---
+++ recipe-frontend/app/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import NavBarStyles from '@/app/styles/components/navbar.module.css';
+
+import Link from "next/link";
+
+import HomeIcon from '@/public/home.svg'
+import AppleIcon from '@/public/apple.svg'
+import HeartIcon from '@/public/heart.svg'
+import CartIcon from '@/public/cart.svg'
+import ProfileIcon from '@/public/profile.svg'
+
+function NavBar() {
+  return (
+    <nav className={NavBarStyles.navbar}>
+        <ul className={NavBarStyles.navlist}>
+            <li className={NavBarStyles.navitem}>
+                <Link href={'./'}><HomeIcon />Home</Link>
+            </li>
+            <li className={NavBarStyles.navitem}>
+                <Link href={'./ingredients'}><AppleIcon />Ingedients</Link>
+            </li>
+            <li className={NavBarStyles.navitem}>
+                <Link href={'./liked-recipes'}><HeartIcon />Liked Recipes</Link>
+            </li>
+            <li className={NavBarStyles.navitem}>
+                <Link href={'./shopping-list'}><CartIcon />Shopping List</Link>
+            </li>
+            <li className={NavBarStyles.navitem}>
+                <Link href={'./profile'}><ProfileIcon />Profile</Link>
+            </li>
+        </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
