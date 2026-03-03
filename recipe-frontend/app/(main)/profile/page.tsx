2d4879c3e5a8f5589caaf8e6ad420fc1866907a0
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-03-03 01:00:00 +0100

    Frontend - Updated profile avatar styling

--- recipe-frontend/app/(main)/profile/page.tsx
+++ recipe-frontend/app/(main)/profile/page.tsx
@@ -6,10 +6,12 @@ import { useContext } from "react";
 import LogoutButton from "@/app/components/LogoutButton";
 import Image from 'next/image';
 
-import ButtonStyles from '@/app/styles/components/button.module.css'
 import ProfileStyles from '@/app/styles/pages/profile.module.css'
-
+import ProfileLink from '@/app/components/ProfileLink';
 import EmptyView from '@/app/components/EmptyView';
+
+import HeartIcon from "@/public/heart.svg";
+import ChefIcon from "@/public/chefhat.svg";
 
 export default function Profile() {
     const auth = useContext(AuthContext);
@@ -24,20 +26,31 @@ export default function Profile() {
         );
     }
 
-
     return (
         <main className={ProfileStyles.profile}>
 
                 <div>
                     <div className={ProfileStyles.profileInfo}>
-                        <Image width={96} height={96} alt={auth.user.username} src={auth.user.avatar || '/avatar.svg'}/>
+                        <Image 
+                            width={96} 
+                            height={96} 
+                            alt={auth.user.username}
+                            className={ProfileStyles.avatar} 
+                            src={auth.user.avatar ? `http://localhost:5041/uploads/avatars/${auth.user.avatar}` : '/avatar.svg'}
+                        />
                         <div className={ProfileStyles.names}>
-                            <p className={ProfileStyles.title}>{auth.user.username}</p>
-                            <p className={ProfileStyles.title}>{auth.user.email}</p>
+                            <p className={ProfileStyles.username}>{auth.user.username}</p>
+                            <p className={ProfileStyles.email}>{auth.user.email}</p>
                         </div>
                         <LogoutButton />
                     </div>
                 </div>
+
+                <div className={ProfileStyles.links}>
+                    <ProfileLink url='/liked-recipes' title='Liked recipes' icon={HeartIcon}/>
+                    <ProfileLink url='/recipes/' title='Your recipes' icon={ChefIcon}/>
+                </div>
+
 
         </main>
     )
