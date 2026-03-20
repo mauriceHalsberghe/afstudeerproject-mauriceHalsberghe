c5d83a61c038875ff361602a851c5f842f4728fa
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-03-20 16:46:58 +0100

    Frontend - added PWA settings

--- recipe-frontend/app/layout.tsx
+++ recipe-frontend/app/layout.tsx
@@ -9,6 +9,15 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Mealio" />
+      </head>
+
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
